cfcd707bf281ce91bac428b732edc9a175167a8d
edwardkoikai <edwardkoikai@gmail.com>
2024-04-26 15:09:53 +0300

    Add list and keys

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 function ColorList() {
   const colors = [
     "firebrick",
@@ -8,19 +6,26 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+const coloCards = colors.map((color, index)=>{
+  return <li key = {index} style={{color: color}}>{color}</li>
+})
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {coloCards}
       </ol>
     </div>
   );
 }
+//Exmaple 2
+const users = [
+  { id: 1, firstName: "Duane", lastName: "Watson" },
+  { id: 2, firstName: "Duane", lastName: "Johnson" },
+];
 
-export default ColorList;
+const userCard = users.map((user)=>{
+  return <h2 key ={user.id}>{user.firstName}{user.lastName}</h2>
+})
+
+export default ColorList
